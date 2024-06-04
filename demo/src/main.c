f6626e495d3f8743b70b709b91f5d34c3a918b44
vadniks <whatever@fake.some>
2024-06-04 13:42:05 +0300

    Structure & logic

--- demo/src/main.c
+++ demo/src/main.c
@@ -21,12 +21,22 @@ enum FontSize {
     FONT_SIZE_DEFAULT = 16
 };
 
+static const int WIDGETS_PER_LIST_PAGE = 8, TOTAL_WIDGETS_IN_LIST = WIDGETS_PER_LIST_PAGE * 3 + 3;
+
 static TTF_Font* gFont = NULL;
 static DlrWidgetsFieldState* gFieldState1 = NULL;
 static DlrWidgetsFieldState* gFieldState2 = NULL;
 static DlrWidgetsFieldState* gFieldState3 = NULL;
+static int gListOffset = 0;
+static SDL_Window* gWindow = NULL;
 
-static void render(void) {
+static void listButtonClicked(int which) {
+    char message[10];
+    assert(SDL_snprintf(message, sizeof message, "Button %d", which) <= (int) sizeof message);
+    assert(SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "List button clicked", message, gWindow) == 0);
+}
+
+static void renderWidgets(void) {
     dlrWidgetsText("Text", FONT_SIZE_DEFAULT, 0, 0);
 
     if (dlrWidgetsButton("Button", FONT_SIZE_DEFAULT, 50, 0))
@@ -43,26 +53,56 @@ static void render(void) {
 
     dlrWidgetsWrappedField(gFieldState3, FONT_SIZE_DEFAULT, 0, 200, 150, 70);
 
-//    dlrPrimitivesPoint(100, 100, 5, 255, 255, 255, 255);
-//    dlrPrimitivesLine(10, 10, 90, 90, 5, 255, 255, 255, 255);
-//    dlrPrimitivesRectangle(110, 110, 100, 50, 1, 255, 255, 255, 255, true);
-//    dlrPrimitivesRectangle(220, 170, 100, 50, 5, 255, 255, 255, 255, false);
-//    dlrPrimitivesCircle(300, 300, 50, 5, 255, 255, 255, 255, false);
-//    dlrPrimitivesCircle(600, 300, 50, 1, 255, 255, 255, 255, true);
-//
-//    SDL_Surface* surface = TTF_RenderUTF8_Blended(gFont, "Hello World!", (SDL_Color) {255, 255, 255, 255});
-//    SDL_Surface* xSurface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA32, 0);
-//    SDL_FreeSurface(surface);
-//    assert(xSurface != NULL);
-//    DlrTexture* texture = dlrTextureCreate(xSurface->w, xSurface->h, xSurface->pixels);
-//    SDL_FreeSurface(xSurface);
-//    dlrPrimitivesTexture(texture, 500, 0, dlrTextureWidth(texture), dlrTextureHeight(texture), 0.0f, 255, 255, 255, 255);
-//    dlrTextureDestroy(texture);
+    // aka list widget:
 
+    dlrPrimitivesRectangle(425, 10, 350, 400, 1, 255, 255, 255, 255, false);
+
+    for (int i = 0; i < WIDGETS_PER_LIST_PAGE; i++) {
+        if (i + gListOffset >= TOTAL_WIDGETS_IN_LIST) break;
+
+        char title[10];
+        assert(SDL_snprintf(title, sizeof title, "Item %d", gListOffset + i) <= (int) sizeof title);
+        dlrWidgetsText(title, FONT_SIZE_DEFAULT, 425, 10 + i * 50);
+
+        dlrWidgetsText("Subtitle", FONT_SIZE_DEFAULT, 425, 30 + i * 50);
+
+        char button[10];
+        assert(SDL_snprintf(button, sizeof button, "Click %d", gListOffset + i) <= (int) sizeof button);
+        if (dlrWidgetsButton(button, FONT_SIZE_DEFAULT, 425 + 260, 20 + i * 50))
+            listButtonClicked(gListOffset + i);
+
+        dlrPrimitivesLine(425, 10 + i * 50, 425 + 350, 10 + i * 50, 1, 200, 200, 200, 200);
+    }
+}
+
+static void renderPrimitives(void) {
+    dlrPrimitivesPoint(100, 100, 5, 255, 255, 255, 255);
+    dlrPrimitivesLine(10, 10, 90, 90, 5, 255, 255, 255, 255);
+    dlrPrimitivesRectangle(110, 110, 100, 50, 1, 255, 255, 255, 255, true);
+    dlrPrimitivesRectangle(220, 170, 100, 50, 5, 255, 255, 255, 255, false);
+    dlrPrimitivesCircle(300, 300, 50, 5, 255, 255, 255, 255, false);
+    dlrPrimitivesCircle(600, 300, 50, 1, 255, 255, 255, 255, true);
+
+    SDL_Surface* surface = TTF_RenderUTF8_Blended(gFont, "Hello World!", (SDL_Color) {255, 255, 255, 255});
+    SDL_Surface* xSurface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA32, 0);
+    SDL_FreeSurface(surface);
+    assert(xSurface != NULL);
+    DlrTexture* texture = dlrTextureCreate(xSurface->w, xSurface->h, xSurface->pixels);
+    SDL_FreeSurface(xSurface);
+    dlrPrimitivesTexture(texture, 500, 0, dlrTextureWidth(texture), dlrTextureHeight(texture), 0.0f, 255, 255, 255, 255);
+    dlrTextureDestroy(texture);
+}
+
+static void render(void) {
+#if true
+    renderWidgets();
+#else
+    renderPrimitives();
+#endif
     SDL_Delay(1000 / 60);
 }
 
-static void loop(SDL_Window* window) {
+static void loop(void) {
     int width, height;
     SDL_Event event;
 
@@ -71,7 +111,7 @@ static void loop(SDL_Window* window) {
     gFieldState3 = dlrWidgetsFieldStateCreate();
 
     while (true) {
-        SDL_GL_GetDrawableSize(window, &width, &height);
+        SDL_GL_GetDrawableSize(gWindow, &width, &height);
         dlrSetViewport(width, height);
 
         while (SDL_PollEvent(&event) == 1) {
@@ -96,12 +136,21 @@ static void loop(SDL_Window* window) {
                 case SDL_TEXTINPUT:
                     dlrTextInput(event.text.text);
                     break;
+                case SDL_MOUSEWHEEL:
+                    if (event.wheel.y > 0) {
+                        if (gListOffset > 0)
+                            gListOffset -= 1;
+                    } else {
+                        if (gListOffset + 1 < TOTAL_WIDGETS_IN_LIST)
+                            gListOffset += 1;
+                    }
+                    break;
             }
         }
 
         dlrUpdateFrame();
         render();
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(gWindow);
     }
     end:
 
@@ -184,7 +233,7 @@ int main(void) {
 
     const int windowMinimalWidth = 16 * 50, windowMinimalHeight = 9 * 50;
 
-    SDL_Window* window = SDL_CreateWindow(
+    gWindow = SDL_CreateWindow(
         "Dnealar",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
@@ -192,10 +241,10 @@ int main(void) {
         windowMinimalHeight,
         SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
-    assert(window != NULL);
-    SDL_SetWindowMinimumSize(window, windowMinimalWidth, windowMinimalHeight);
+    assert(gWindow != NULL);
+    SDL_SetWindowMinimumSize(gWindow, windowMinimalWidth, windowMinimalHeight);
 
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    SDL_GLContext glContext = SDL_GL_CreateContext(gWindow);
 
     SDL_GL_SetSwapInterval(1);
 
@@ -210,11 +259,11 @@ int main(void) {
         textureData,
         textMetrics
     );
-    loop(window);
+    loop();
     dlrQuit();
 
     SDL_GL_DeleteContext(glContext);
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(gWindow);
 
     TTF_CloseFont(gFont);
     TTF_Quit();
